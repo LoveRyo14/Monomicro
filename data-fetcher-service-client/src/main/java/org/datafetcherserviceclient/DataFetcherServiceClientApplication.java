bb19b789702375fb00e5eebbb5ee578726d17983
Yaroslav <sidoruka56@gmail.com>
2024-12-30 09:51:23 +0200

    Created Eureka Server module and DataFetcherServiceClient module, all working correctly.

---
+++ data-fetcher-service-client/src/main/java/org/datafetcherserviceclient/DataFetcherServiceClientApplication.java
@@ -0,0 +1,13 @@
+package org.datafetcherserviceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFetcherServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataFetcherServiceClientApplication.class, args);
+	}
+
+}
---
+++ data-fetcher-service-client/src/test/java/org/datafetcherserviceclient/DataFetcherServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.datafetcherserviceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFetcherServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
